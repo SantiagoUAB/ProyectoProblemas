f64d6606c81c0f3dec57a6448455cf096bcdec3d
ArmanPipoyan <64909828+ArmanPipoyan@users.noreply.github.com>
2020-11-13 15:11:23 +0100

    Test CreateShape Class Piece implemented

--- src/main/java/ProyectoProblemas/Piece.java
+++ src/main/java/ProyectoProblemas/Piece.java
@@ -1,17 +1,28 @@
 package ProyectoProblemas;
 
-public abstract class Piece {
+public class Piece {
+	private int[][] shape;
+	private int shapeType;
+	private static int[][][] shapes = {
+			{{0,0}, {0,1}, {1,1}, {1,0}},  //O
+			{{0,0}, {0,1}, {0,2}, {0,3}},  //I
+			{{0,0}, {0,1}, {0,2}, {1,0}},  //L
+			{{-1,1}, {0,1}, {0,0}, {1,0}}, //Z
+			{{0,0}, {1,0}, {1,1}, {1,2}},  //J
+			{{0,0}, {-1,1}, {0,1}, {1,1}}, //T
+			{{0,0}, {1,0}, {1,1}, {2,1}},  //S
+	};
 	
+	public Piece(int shapeType) {
+		this.shapeType = shapeType;
+		shape = shapes[shapeType];
+	}
 	
+	public int[][] getShape() {
+		return shape;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public int getShapeType() {
+		return shapeType;
+	}
 }
--- src/test/java/ProyectoProblemas/PieceTest.java
+++ src/test/java/ProyectoProblemas/PieceTest.java
@@ -7,13 +7,15 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class PieceTest {
-
 	@Test
 	public void testCreateShape() {
-		Piece piece = new Piece('j');
+		int inputType = 4;
+		Piece piece = new Piece(inputType);
 		int[][] testPiece = {{0,0},{1,0},{1,1},{1,2}};
 		int[][] jpiece = piece.getShape();
+		int shapeType = piece.getShapeType();
 		assertTrue(Arrays.deepEquals(jpiece, testPiece));
+		assertTrue(shapeType == inputType);
 	}
 
 }
