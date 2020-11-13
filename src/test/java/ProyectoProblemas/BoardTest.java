4949fde41cd7260bc1e0844c0ed76e2d8b261d61
ArmanPipoyan <64909828+ArmanPipoyan@users.noreply.github.com>
2020-11-13 16:47:05 +0100

    Test RemoveCompleteLines added

--- src/test/java/ProyectoProblemas/BoardTest.java
+++ src/test/java/ProyectoProblemas/BoardTest.java
@@ -1,15 +1,22 @@
 package ProyectoProblemas;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
 public class BoardTest {
+	Board board;
+	
+	@Before
+	public void setUp() throws Exception {
+		board = new Board();
+	}
+	
 	
     @Test
     public void boardTest() {
-    	Board board = new Board();
     	int rows = board.getRows();
     	int columns = board.getColumns();
     	assertTrue(rows==20);
@@ -18,9 +25,20 @@ public class BoardTest {
     
     @Test
     public void boardZeroTest() {
-    	Board board = new Board();
     	int[][] boardArray = board.getBoard();
     	int[][] zeroArray = new int[20][10];
     	assertTrue(Arrays.deepEquals(zeroArray, boardArray));
+    }
+    
+    @Test
+    public void completeLineTest() {
+    	for(int i = 0; i < board.getColumns(); i++) {
+    		board.setValueToCoord(board.getRows(), i, 1);
+    		board.setValueToCoord(board.getRows(), i, 1);
+    	}
+    	int[][] zeroArray = new int[20][10];
+    	assertFalse(Arrays.deepEquals(zeroArray, board.getBoard()));
+    	board.eliminateCompleteRows();
+    	assertTrue(Arrays.deepEquals(zeroArray, board.getBoard()));
     }
 }
