2fff7f68632a6e734b89718eaa647fa77eb61b1e
Alejandro <AlejandroPerezma@users.noreply.github.com>
2020-11-13 17:52:07 +0100

    Test RotateShape
    Test Failed

--- src/main/java/ProyectoProblemas/Piece.java
+++ src/main/java/ProyectoProblemas/Piece.java
@@ -25,4 +25,6 @@ public class Piece {
 	public int getShapeType() {
 		return shapeType;
 	}
+	
+	
 }
--- src/test/java/ProyectoProblemas/PieceTest.java
+++ src/test/java/ProyectoProblemas/PieceTest.java
@@ -3,19 +3,37 @@ package ProyectoProblemas;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class PieceTest {
+	Piece piece;
+	int inputType;
+	
+	@Before
+	public void setUp() throws Exception{
+		inputType = 4;
+		piece = new Piece(inputType);
+	}
+	
 	@Test
-	public void testCreateShape() {
-		int inputType = 4;
-		Piece piece = new Piece(inputType);
+	public void testCreateShape() {		
 		int[][] testPiece = {{0,0},{1,0},{1,1},{1,2}};
 		int[][] jpiece = piece.getShape();
 		int shapeType = piece.getShapeType();
 		assertTrue(Arrays.deepEquals(jpiece, testPiece));
 		assertTrue(shapeType == inputType);
+	}
+	
+	@Test
+	public void testRotateShape() {
+		int[][] testPiece = {{0,0},{0,-1},{1,-1},{2,-1}};
+		piece.rotateShape();
+		int[][] jpiece = piece.getShape();
+		for (int a=0; a<4; a++) {
+			System.out.println(Arrays.toString(jpiece[a]));
+		}
+		assertTrue(Arrays.deepEquals(jpiece, testPiece));
 	}
 
 }
