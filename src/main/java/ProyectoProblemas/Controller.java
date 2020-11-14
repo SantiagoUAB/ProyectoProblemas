066db7f6c846f7bed01c3c29a636587e6fb49966
Alejandro <AlejandroPerezma@users.noreply.github.com>
2020-11-14 13:10:59 +0100

    Some changes to fix

--- src/main/java/ProyectoProblemas/Controller.java
+++ src/main/java/ProyectoProblemas/Controller.java
@@ -42,7 +42,7 @@ public class Controller {
 	
 	private int generateRandomShape() {
 		Random rand = new Random();
-		int res = Math.abs(rand.nextInt()) % 6 + 1;
+		int res = Math.abs(rand.nextInt()) % 7;
 		return res;
 	}
 	
